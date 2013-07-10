8318046ee8d2e19fc76e583ef62a53d5c5decc79
zlocket1 <zachls17@gmail.com>
2013-07-10 19:36:58 +0000

    Automatic categorization by submission link is reflected on video viewing page

--- swat_additions/views.py
+++ swat_additions/views.py
@@ -4,6 +4,8 @@ from swat_additions import forms
 from localtv.submit_video.views import SubmitURLView, SubmitVideoView
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from localtv import models
+from django.conf import settings
 
 class CatSubmitURLView(SubmitURLView):
 	form_class = forms.CatSubmitURLForm
@@ -20,14 +22,23 @@ class CatSubmitVideoView(SubmitVideoView):
 	categories_list = []
 	
 	def get(self, request, *args, **kwargs):
+		# Pulls in categories from request, and splits
 		if "categories" in request.GET:
-			self.categories_list = request.GET["categories"].split('/')
-			for category in self.categories_list:
-				print 'Category:',category
+			temp_categories = request.GET["categories"].split('/')
+			
+			allCats = models.Category.objects.filter(site=settings.SITE_ID)
+			self.categories_list = []
+			for thisCat in allCats:
+				if thisCat.slug in temp_categories:
+					self.categories_list.append(thisCat.id)
+		
+			# for category in self.categories_list:
+			#	print 'Category:',category
 		return super(CatSubmitVideoView, self).get(request, *args, **kwargs)
 		
 	
 	def get_initial(self):
+		# Checks checkboxes
 		initial = super(CatSubmitVideoView, self).get_initial()
 		#print ("INITIAL?", request.GET)
 		#print "CATEGORIES", self.categories_list
